25f0e56a3587cbbd078dfff40d73ff76dd107610
Gyan <hi.gyanaranjanpatra@gmail.com>
2025-08-24 13:02:57 +0530

    fix(auth): improve error handling and loading state in signin page

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,21 +4,20 @@ import {
   Card,
   CardContent,
   CardDescription,
-
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
-import {useState } from "react";
+import { useState } from "react";
 import toast, { LoaderIcon, Toaster } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 import { signIn } from "next-auth/react";
 
 export default function Page() {
-    const router = useRouter()
+  const router = useRouter();
   const [isLoading, Setloading] = useState(false);
   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -27,17 +26,24 @@ export default function Page() {
     const password = formData.get("password");
     try {
       Setloading(true);
-      await signIn("credentials", {
+      const res = await signIn("credentials", {
         redirect: false,
         email: email,
         password: password,
       });
-      toast.success("Signed in successful");
-      router.push("/");
-    } catch (error) {      
-        const err = error as Error 
+
+      if (res?.error) {
+        console.error(res.error);
+        toast.error("Login Failed");
+        Setloading(false);
+      } else {
+        toast.success("Login successful");
+        router.push("/");
+      }
+    } catch (error) {
+      const err = error as Error;
       Setloading(false);
-      toast.error(err.message)
+      toast.error(err.message);
     }
   };
   return (
@@ -68,13 +74,17 @@ export default function Page() {
           </div>
           <div className="pt-10 flex flex-col gap-4 justify-center items-center">
             <Button type="submit" className="w-full">
-          {
-            isLoading?(<div className="flex gap-2 items-center justify-center"><h1>Signingin </h1> <LoaderIcon className="animate-spin"/></div>):"Login" 
-          }
-        </Button>
-        <Link href="/signup">
-          Dont have account ? <span className="text-primary">Signup</span>
-        </Link>
+              {isLoading ? (
+                <div className="flex gap-2 items-center justify-center">
+                  <h1>Signingin </h1> <LoaderIcon className="animate-spin" />
+                </div>
+              ) : (
+                "Login"
+              )}
+            </Button>
+            <Link href="/signup">
+              Dont have account ? <span className="text-primary">Signup</span>
+            </Link>
           </div>
         </form>
       </CardContent>
